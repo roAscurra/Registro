f0f68a61dcb48f1ab39bf318902068a30302703c
roAscurra <rominaascurra4887@gmail.com>
2023-06-26 19:46:39 -0300

    Funcion para crear el usuario

--- firebase.js
+++ firebase.js
@@ -23,3 +23,53 @@
  const app = initializeApp(firebaseConfig);
  const db = getDatabase(app);
  const auth = getAuth(app);
+
+ export const registrar = (email, password) => {
+    createUserWithEmailAndPassword(auth,nombre, apellido, fechaNacimiento, dni, celular, email, password)
+      .then((userCredential) => {
+        console.log('Usuario registrado!', auth);
+        const user = userCredential.user;
+        // Enviar el correo electrónico de verificación
+        sendEmailVerification(user)
+          .then(() => {
+            console.log('Correo de verificación enviado');
+          })
+          .catch((error) => {
+            console.log('Error al enviar el correo de verificación:', error);
+          });
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+    // Listener de cambios en el estado del token de identificación
+    onIdTokenChanged(auth, (user) => {
+      if (user) {
+        user.getIdTokenResult()
+          .then((idTokenResult) => {
+            const emailVerified = idTokenResult.claims.email_verified;
+            // Guardar el estado de verificación en la base de datos
+            const userRef = ref(db, `Usuarios/${user.uid}`);
+            update(userRef, {  
+              Verificado: emailVerified,
+              Email: email,            
+              Nombre: nombre,
+              Apellido: apellido,
+              Nacimiento: fechaNacimiento,
+              Dni: dni,
+              Celular: celular,
+            })
+              .then(() => {
+                console.log('Estado de verificación actualizado en la base de datos');
+              })
+              .catch((error) => {
+                console.log('Error al actualizar el estado de verificación en la base de datos:', error);
+              });
+          })
+          .catch((error) => {
+            console.log('Error al obtener el estado de verificación del correo electrónico:', error);
+          });
+      }
+    });
+  };
